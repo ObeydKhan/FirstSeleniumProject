d0451f2e88c97eb7ad9cc9f2456ec350de28706b
ObeydKhan <kobeydullah@yahoo.com>
2018-06-10 15:05:36 -0400

    Marufjon

--- src/com/cbt/GoogleSearch.java
+++ src/com/cbt/GoogleSearch.java
@@ -6,9 +6,21 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class GoogleSearch {
 
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", "C:/Users/obeyd/Documents/selenium dependencies/drivers/ChromeDriver.exe");
-		WebDriver driver=new ChromeDriver();
-		driver.get("https://www.etsy.com/search?q=wooden%20spoon");
+		System.setProperty("webdriver.chrome.driver", "C:/Users/obeyd/Documents/selenium dependencies/drivers/ChromeDriver.exe");//( actual address of driver)
+		WebDriver driver=new ChromeDriver(); // open the browser
+		driver.get("https://www.amazon.com"); //goes to a website
+		String expected="Google"; 
+		String actual=driver.getTitle(); //gets the title of the tab
+		
+		if(expected.equals(actual)) {
+			System.out.println("Passed");
+		}
+		else {
+			System.out.println("Failed");
+		System.out.println("Expected:\t"+expected);
+		System.out.println("Actual:\t"+actual);
+		}
+		
 		
 	}
 }
